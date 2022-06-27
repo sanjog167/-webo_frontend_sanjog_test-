89d9df489cd639d58bf27916da9e52df505c8458
Sanjog Piya <sanjogpiya6@gmail.com>
2022-06-27 09:39:04 +0545

    Statisticsissuefix

--- src/StatsData.js
+++ src/StatsData.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function StatsData({title,image,description}) {
   return (
-    <div className='col-lg-4'>
+    <div className='col-lg-4 mb-2'>
     <div className={'stats_1'}>
         <h1 className='fs-78'>{title}</h1>
         <div className='d-flex align-items-center'>
